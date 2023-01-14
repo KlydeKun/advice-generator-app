2c79aef3eb9882e426a46c590fcd391151b7505b
klyde <kylelerio15@gmail.com>
2023-01-14 10:04:28 +0800

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+
+import pauseMobile from "./images/pattern-divider-mobile.svg";
+import pauseDesktop from "./images/pattern-divider-desktop.svg";
+import dice from "./images/icon-dice.svg";
+import "./index.css";
+
+function App() {
+  const [text, setText] = useState([])
+
+  const fetchAdvice = async() => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+
+    console.log(data)
+
+    setText(data.slip)
+  }
+
+  useEffect(() => {
+    fetchAdvice()
+  }, [], {once: true})
+
+  return (
+    <>
+      <div className="container">
+        <h1>Advice #{text.id}</h1>
+        <p>"{text.advice}"</p>
+
+        <picture>
+          <source media="(min-width: 768px)" srcSet={pauseDesktop} />
+          <img src={pauseMobile} alt="" />
+        </picture>
+
+        <div>
+          <button onClick={fetchAdvice}>
+            <img src={dice} alt="" />
+          </button>
+        </div>
+      </div>
+      <div className = "attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        .&nbsp;&nbsp;Coded by <a href="#">Kyle Rivera Lerio</a>.
+      </div>
+    </>
+  );
+}
+
+export default App;
